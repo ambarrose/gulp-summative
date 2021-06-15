43a46afcf7c7982d945a1934dc45ee47b68a364e
Ambar Williams <270030198@bun2-206-11.acg.ac.nz>
2021-06-15 12:28:30 +1200

    second push

--- js/custom.js
+++ js/custom.js
@@ -1,39 +1,30 @@
-// custom js
+// custom js ......
 
 $(document).ready(function() {
+
   $('#fullpage').fullpage({
     //Navigation
     menu: '#menu',
     lockAnchors: true,
-    anchors:['firstPage', 'secondPage'],
-    navigation: true,
-    navigationPosition: 'right',
-    navigationTooltips: ['', 'Step one', 'Step two', 'Step three', 'Step four', 'Step five', 'Step six', 'Complete'],
-    showActiveTooltip: true,
+    anchors:['section1', 'slide1', 'slide2', 'slide3'],
+    controlArrows: true,
     slidesNavigation: true,
     slidesNavPosition: 'bottom'
   });
+  // fullpage ENDS
 
   // Click Events Fullpage.js provides an API
-  $('#start').click(function (){
-    // To move up one slide
-    $.fn.fullpage.moveSectionDown();
-  });
-
-  $('#next').click(function (){
+  $('#btnOne').click(function (){
     // To move down one slide...
     $.fn.fullpage.moveSectionDown();
   });
 
-  $('#back').click(function (){
+  $('#btnTwo').click(function (){
     // To move up one slide
     $.fn.fullpage.moveSectionUp();
   });
 
-
-  // if(peopleFromDom < data.hotel.guestMin)
-
-
-
 });
-// document ENDS
+// document ready ENDS
+
+// if(peopleFromDom < data.hotel.guestMin)
